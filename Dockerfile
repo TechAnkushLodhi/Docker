ea9f8e750e2e082cf9e62f1ac70050fa79cbe7d1
ankushlodhi <ankush@icecubedigital.com>
2025-11-21 18:23:26 +0530

    Initial commit: Magento Docker setup

--- Dockerfile
+++ Dockerfile
@@ -1,23 +1,51 @@
 # PHP 8.4 FPM base image
 FROM php:8.4-fpm
 
-# Install basic dependencies और PHP extensions
+# Install OS dependencies required for all PHP extensions
 RUN apt-get update && apt-get install -y \
     git \
     unzip \
     libzip-dev \
     libpng-dev \
+    libjpeg62-turbo-dev \
+    libfreetype6-dev \
     libicu-dev \
     libxml2-dev \
-    && docker-php-ext-install \
-        zip \
-        intl \
-        pdo_mysql \
-        gd \
-        xml
+    libonig-dev \
+    libxslt1-dev \
+    libcurl4-openssl-dev \
+    libssl-dev \
+    pkg-config \
+    && apt-get clean && rm -rf /var/lib/apt/lists/*
 
-# Work directory
+# Configure GD with JPEG & Freetype support
+RUN docker-php-ext-configure gd \
+    --with-jpeg \
+    --with-freetype
+
+# Install PHP extensions
+RUN docker-php-ext-install \
+    zip \
+    intl \
+    pdo_mysql \
+    gd \
+    xml \
+    bcmath \
+    soap \
+    ftp \
+    sockets \
+    xsl \
+    mbstring \
+    ctype \
+    simplexml \
+    iconv
+
+    # Increase memory limit
+RUN echo "memory_limit=5G" > /usr/local/etc/php/conf.d/memory-limit.ini
+
+# Install Composer 2.8 (official method)
+COPY --from=composer/composer:2.8-bin /composer /usr/bin/composer
+
 WORKDIR /var/www/html
 
-# Container start message (optional)
 CMD ["php-fpm"]
---
+++ src/app/etc/env.php
@@ -0,0 +1,89 @@
+<?php
+return [
+    'backend' => [
+        'frontName' => 'admin_docker'
+    ],
+    'remote_storage' => [
+        'driver' => 'file'
+    ],
+    'cache' => [
+        'graphql' => [
+            'id_salt' => 'LX4QT0DXnzZdvbsxW77sh91SAD4sw2vT'
+        ],
+        'frontend' => [
+            'default' => [
+                'id_prefix' => '69d_'
+            ],
+            'page_cache' => [
+                'id_prefix' => '69d_'
+            ]
+        ],
+        'allow_parallel_generation' => false
+    ],
+    'config' => [
+        'async' => 0
+    ],
+    'queue' => [
+        'consumers_wait_for_messages' => 1
+    ],
+    'crypt' => [
+        'key' => 'base64Jyjxod+AqkzkAGIxdjAeg4fY9loIwNRniwgz2KOpajw='
+    ],
+    'db' => [
+        'table_prefix' => '',
+        'connection' => [
+            'default' => [
+                'host' => 'mysql',
+                'dbname' => 'magento',
+                'username' => 'magento',
+                'password' => 'magentopass',
+                'model' => 'mysql4',
+                'engine' => 'innodb',
+                'initStatements' => 'SET NAMES utf8;',
+                'active' => '1',
+                'driver_options' => [
+                    1014 => false
+                ]
+            ]
+        ]
+    ],
+    'resource' => [
+        'default_setup' => [
+            'connection' => 'default'
+        ]
+    ],
+    'x-frame-options' => 'SAMEORIGIN',
+    'MAGE_MODE' => 'developer',
+    'session' => [
+        'save' => 'files'
+    ],
+    'lock' => [
+        'provider' => 'db'
+    ],
+    'directories' => [
+        'document_root_is_pub' => true
+    ],
+    'cache_types' => [
+        'config' => 1,
+        'layout' => 1,
+        'block_html' => 1,
+        'collections' => 1,
+        'reflection' => 1,
+        'db_ddl' => 1,
+        'compiled_config' => 1,
+        'eav' => 1,
+        'customer_notification' => 1,
+        'config_integration' => 1,
+        'config_integration_api' => 1,
+        'graphql_query_resolver_result' => 1,
+        'full_page' => 1,
+        'config_webservice' => 1,
+        'translate' => 1
+    ],
+    'downloadable_domains' => [
+        'docker.localhost.com'
+    ],
+    'install' => [
+        'date' => 'Fri, 21 Nov 2025 06:29:31 +0000'
+    ]
+];
